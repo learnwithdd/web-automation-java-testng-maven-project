26e921cd340b58462553b8dd55332d7aab8cd4d4
dnyaneshwardivekar.qa@gmail.com <dnyaneshwardivekar.qa@gmail.com>
2026-03-08 22:41:30 +0530

    "Browser Fix"

--- src/test/java/com/lwdd/tests/BaseTest.java
+++ src/test/java/com/lwdd/tests/BaseTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 	
 WebDriver driver;
@@ -16,12 +18,16 @@ WebDriver driver;
 	public void initBrowser() {		
 		// open the chrome browser 		
 		ChromeOptions options = new ChromeOptions();
+
 		options.addArguments("--headless=new");
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
 		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--remote-allow-origins=*");
 
-		driver = new ChromeDriver(options);	
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(options);
 //		Open page
 		driver.get("https://practicetestautomation.com/practice-test-login/");	
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
