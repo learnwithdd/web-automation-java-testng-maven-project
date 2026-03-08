c0344919f169de88a5b072b5c12ff7cc65c5256b
dnyaneshwardivekar.qa@gmail.com <dnyaneshwardivekar.qa@gmail.com>
2026-03-08 22:21:24 +0530

    "Test Case Update"

--- src/test/java/com/lwdd/tests/LogoutTest.java
+++ src/test/java/com/lwdd/tests/LogoutTest.java
@@ -6,47 +6,50 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LogoutTest extends BaseTest {
-	
-WebDriver driver;
-	
+import com.lwdd.pages.HomePage;
+import com.lwdd.pages.LoginPage;
 
-	
-	
-@Test(description = "Test case 3: LogoutTest test", priority = 3)
- public void userLogoutTest() {
-		
+public class LogoutTest extends BaseTest {
+
+
+	@Test(description = "Test case 3: LogoutTest test", priority = 3)
+	public void userLogoutTest() {
+
 //		Test case 3: LogoutTest test
-	
-//		Type username student into Username field
-		driver.findElement(By.xpath("//input[@id=\"username\"]")).sendKeys("student");		
-		
-//		Type password Password123 into Password field
-		driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("Password123");
-		
-//		Push Submit button submit
-		driver.findElement(By.xpath("//button[@id='submit']")).click();		
-		
+
+		LoginPage loginPage = new LoginPage(driver);
+		HomePage homePage = new HomePage(driver);
+
+//Type username student into Username field		
+		loginPage.enterUsername("student");
+
+//Type password Password123 into Password field
+		loginPage.enterPassword("Password123");
+
+//Push Submit button submit
+		loginPage.clickOnSubmitButton();
+
 //		Verify new page URL contains practicetestautomation.com/logged-in-successfully/
-		
-		String actUrl = driver.getCurrentUrl();		
+
+		String actUrl = driver.getCurrentUrl();
 		Assert.assertEquals(actUrl, "https://practicetestautomation.com/logged-in-successfully/");
-		
+
 //		Verify new page contains expected text ('Congratulations' or 'successfully logged in')
 
-		String actText = driver.findElement(By.xpath("//strong[text()='Congratulations student. You successfully logged in!']")).getText();
+		String actText = driver
+				.findElement(By.xpath("//strong[text()='Congratulations student. You successfully logged in!']"))
+				.getText();
 		Assert.assertEquals(actText, "Congratulations student. You successfully logged in!");
-		
+
 //		Verify button Log out is displayed on the new page
 		WebElement logoutButton = driver.findElement(By.xpath("//a[text()='Log out']"));
-		
-		boolean status = logoutButton.isDisplayed();		
+
+		boolean status = logoutButton.isDisplayed();
 		Assert.assertTrue(status);
-		
+
 //      click on the Logout button 		
 		logoutButton.click();
-		
+
 	}
-	
 
 }
